85116eb140a66f80dd323449268c1430b52fd06c
Josh Frechette <josh.f.design@gmail.com>
2019-12-10 14:19:43 -0500

    Possible fix to heroku flag error.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var fs = require("fs");
 var http = require("http");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
