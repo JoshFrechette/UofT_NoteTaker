44a67fe9b6bed36ff683df82f20baa1ecfe116d9
Josh Frechette <josh.f.design@gmail.com>
2019-12-13 15:50:42 -0500

    Woork done on the display note function(incomplete).

--- server.js
+++ server.js
@@ -27,12 +27,16 @@ app.post("/api/notes", function (req, res) {//Get the text from the html
   title = newnote.title;
   text = newnote.text;
   fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-    if (err) { throw err }
+    if (err) { 
+      throw err 
+    }
     noteObj = JSON.parse(data);
     id = getId();
     noteObj.push({ title, text, id: id });
     fs.writeFile(__dirname + '/db/db.json', JSON.stringify(noteObj), (err) => {
-      if (err) { throw err }
+      if (err) { 
+        throw err 
+      }
     })
     return res.json(JSON.parse(data))
   })
@@ -50,7 +54,7 @@ app.delete("/api/notes/:id", function (req, res) {
       }
       noteObj.splice(i, 1);
     }
-    fs.writeFile('db/db.json', noteObj, (err) => {
+    fs.writeFile('db/db.json',"utf8", noteObj, (err) => {
       if (err) {
         throw err
       }
@@ -61,12 +65,19 @@ app.delete("/api/notes/:id", function (req, res) {
 
 //Display stored note(s)
 app.get("/api/notes", function (req, res) {
-  fs.readFile(__dirname + "/db/db.json", function (err, data) {
-    if (err) { throw err }
-    console.log(data)
-    return res.send(JSON.parse(data))
+
+  fs.readFileSync(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    content = data;
+    updateNotes()
   })
 });
+
+function updateNotes(){
+  return res.json(content);
+}
 
 //Create a unique id# for every note entry
 function getId() {
