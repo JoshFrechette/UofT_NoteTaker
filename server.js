c617ae6820d6c212190545acc7fa9c590c66efef
Josh Frechette <josh.f.design@gmail.com>
2019-12-13 14:44:18 -0500

    getId now producing longer but unique id numbers.

--- server.js
+++ server.js
@@ -7,91 +7,73 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 //Basic Get Functions
 //HTML routes
-app.get("/notes", function (req, res){
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/", function (req, res){
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-//Display stored note(s)
-app.get("/api/notes", async function(req, res){
-  await fs.readFile(__dirname + "/db/db.json", function (err, data) {
-      if (err) throw err;
-      return res.json(JSON.parse(data))
-  })
 });
 
 //Save a note
 app.post("/api/notes", function (req, res) {//Get the text from the html
   let newnote = req.body;
-    title = newnote.title;
-    text = newnote.text;
+  title = newnote.title;
+  text = newnote.text;
   fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-      noteObj = JSON.parse(data);
-      id = getId();
-      noteObj.push({title, text ,id: id});
-      fs.writeFile(__dirname + '/db/db.json', JSON.stringify(noteObj),(err) => {
-        if (err) {
-          throw err;
-        }
-        return res.json(JSON.parse(data))
-      })
+    if (err) { throw err }
+    noteObj = JSON.parse(data);
+    id = getId();
+    noteObj.push({ title, text, id: id });
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(noteObj), (err) => {
+      if (err) { throw err }
+    })
+    return res.json(JSON.parse(data))
   })
 })
-    
+
 // Delete a note
 app.delete("/api/notes/:id", function (req, res) {
   let id = req.params.id;
   fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
     let noteObj = JSON.parse(data);
-    console.log(id)
-    noteObj.splice(id, 1); //Is taking out every element after the selection 
-    newNoteObj = JSON.stringify(noteObj);
-    console.log(newNoteObj)
-    // function checkId(){
-
-    // }
-      fs.writeFile('db/db.json',newNoteObj,(err)=>{
-          if (err) {
-            throw err     
-          }  
-          return res.json(JSON.parse(data))
-      })
+    console.log(noteObj)
+    for (i = 0; i < noteObj.length - 1; i++) {
+      if (noteObj[i].id === id) {
+        return i
+      }
+      noteObj.splice(i, 1);
+    }
+    fs.writeFile('db/db.json', noteObj, (err) => {
+      if (err) {
+        throw err
+      }
+    })
+    return res.json(JSON.parse(data))
   })
 });
 
 //Display stored note(s)
-app.get("/api/notes", function(req, res){
+app.get("/api/notes", function (req, res) {
   fs.readFile(__dirname + "/db/db.json", function (err, data) {
-      if (err) {
-        throw err;
-      }
-      return res.json(JSON.parse(data))
+    if (err) { throw err }
+    console.log(data)
+    return res.send(JSON.parse(data))
   })
 });
 
 //Create a unique id# for every note entry
 function getId() {
-  noteEntries = JSON.parse(fs.readFileSync(__dirname + '/db/db.json', 'utf8'));
-  console.log(noteEntries)
-  if (noteEntries.length === null) {
-    return 1
-  } else {
-    return noteEntries.length +1
-  }
+  let id = (Math.floor(Math.random() * 100000));
+  return id
 };
 
-app.listen(PORT, function (){
-    console.log("Server listening to Note-Taker on http://localhost:" + PORT);
+app.listen(PORT, function () {
+  console.log("Server listening to Note-Taker on http://localhost:" + PORT);
 })
