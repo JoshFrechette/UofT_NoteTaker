5aef4b32693427839498cec6b1bc45d481d8883a
Josh Frechette <josh.f.design@gmail.com>
2020-02-05 02:57:23 -0500

    Delete note function appears to be fixed.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,21 +4,37 @@ var fs = require("file-system");
 // var fs = require("fs");
 var path = require("path");
 
+fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    noteObj = JSON.parse(data);
+    return noteObj
+})
+
 
 module.exports = function (app) {
 
     //Save a note
     app.post("/api/notes", function (req, res) {//Get the text from the html
-        let newNote = req.body;
+        console.log(req.body)
+        let newNote = {};
         newNote.id = getId();
-
+        newNote.title = req.body.title;
+        newNote.text = req.body.text;
+  
+        // newNote.id += ":" + {title: req.body.title, text: req.body.text};
+        // let id = getId();
+        // let newNote = "{" + id + ":{" + "title:" + req.body.title + "," + "text:" + req.body.text + "}";
+        // console.log(JSON.stringify(newNote))
+        console.log(newNote)
         fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
             if (err) {
                 throw err
             }
             noteObj = JSON.parse(data);
             noteObj.push(newNote);
-            fs.writeFile(path.join(__dirname,'../db/db.json'), JSON.stringify(noteObj), (err) => {
+            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(noteObj), (err) => {
                 if (err) {
                     throw err
                 }
@@ -26,62 +42,46 @@ module.exports = function (app) {
             db.push(noteObj)
             // return res.json(JSON.parse())
         })
-        noteObj = {};
+        // noteObj = {};
     })
 
-
-        // Delete a note
-        app.delete("/api/notes/:id", function (req, res) {
-
-           // console.log("req" + req.params.id)
-            let id = req.params.id
-            console.log("notes id " + id)
-            // fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
-            //     if (err) {
-            //         throw err;
-            //     }
-            //     let noteObj = JSON.parse(data);
-            //     console.log("object before " + noteObj[0].title)
-
-                
-                for (i = 0; i < noteObj.length; i++) {
-                    // console.log("id from the click " + id)
-                    if (noteObj[i].id === id) {
-                     return i
-                    }
-                    console.log("index position " + i)
+    // Delete a note
+    app.delete("/api/notes/:id", function (req, res) {
+        let id = JSON.parse(req.params.id);
+            console.log(noteObj[0].id)
+            console.log(id)
+            for (i = 0; i <= noteObj.length; i++) {
+                if (noteObj[i].id === id) {
                     noteObj.splice(i, 1);
-                    console.log("object after " + noteObj)
+                    console.log(noteObj)
+                    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(noteObj), (err) => {
+                        if (err) {
+                            throw err
+                        }
+                        console.log("this is post noteObj " + noteObj)
+                        db =[];
+                        db.push(noteObj)
+                        return res.json(noteObj)
+                    })
                 }
+            }
+    })
 
-                fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(noteObj), (err, data) => {
-                    if (err) {
-                        throw err
-                    }
-                    console.log(data)
-                    // return res.json(JSON.parse(data))
-                })
-                res.end()
-            });
-        
-
-        //Display stored note(s)
-        app.get("/api/notes", function (req, res) {
-            // return res.json(db)
-            fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                return res.json(JSON.parse(data))
-            })
-        });
-    }
-
-
+    //Display stored note(s)
+    app.get("/api/notes", function (req, res) {
+        // return res.json(db)
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            return res.json(JSON.parse(data))
+        })
+    });
+}
 
 //Create a unique id# for every note entry
 function getId() {
-            let id = (Math.floor(Math.random() * 100000));
+    let id = (Math.floor(Math.random() * 100000));
 
-            return id
-        };
+    return id
+};
